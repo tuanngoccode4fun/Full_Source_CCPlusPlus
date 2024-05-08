98de5444be4e17bef1f7aaea9ea80319b25d1b41
Admin <Admin@ADMIN>
2024-05-08 21:59:07 +0700

    - Update main file replace . -> [.]

---
+++ 00_C_Language/300_Baitapthieunhi/bai_1_Replace_Text/main.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include <string.h>
+char *text = "255.1.1.0";
+char array[100];
+int getsizeofcharpoint(char * input)
+{
+    int i = 0;
+    while (*(input + i))
+    {
+        i++;
+    }
+    return i;
+    
+}
+void findIndexCom(char *textInput, int length)
+{
+    printf("Input: %s \r\n", text);
+    printf("Length: %d \r\n", length);
+    int j=0,i = 0;
+    for(i = 0; i<= length; i++)
+    {
+        if(text[i] == '.')
+        {
+            array[j]='[';
+            j++;
+            array[j]='.';
+            j++;
+            array[j]=']';
+            printf("index = %d \r\n", i);
+        }
+        else
+        {
+            array[j]=text[i];
+        }
+            j++;
+    }
+    printf("Output: %s \r\n",array);
+}
+int main()
+{
+    findIndexCom(text,getsizeofcharpoint(text));
+
+    return 0;
+}
